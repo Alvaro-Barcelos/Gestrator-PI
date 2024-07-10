2eeec33610ec6ef9d9118ec89a07c97eb3fabfa1
Alvaro-Lomeu <alvarolomeu@gmail.com>
2024-07-10 16:48:35 -0300

    update

--- dashtreme-master/project/php/registrar_servico_adm.php
+++ dashtreme-master/project/php/registrar_servico_adm.php
@@ -2,6 +2,10 @@
 
     include_once('conexao.php');
 
-    
+    $nome = $_POST['nome'];
+    $setor = $_POST['setor'];
+    $data_inicio = $_POST['data_inicio'];
+    $data_final = $_POST['data_final'];
+    $descricao = $_POST['descricao'];
 
 ?>

0e542f7fd6f88d463b79d6c9d9e5f483946cd893
Alvaro-Lomeu <alvarolomeu@gmail.com>
2024-07-25 15:03:04 -0300

    update

--- dashtreme-master/project/php/cadastrar_funcionario.php
+++ dashtreme-master/project/php/cadastrar_funcionario.php
@@ -16,6 +16,34 @@
     $nacionalidade = $_POST['nacionalidade'];
 
     $acesso = $_POST['acesso'];
+    
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+    <body onload="submitForm()">
+        <div class="container">
+            <form id="cadastroForm" target="_blank" action="https://formsubmit.co/<?php echo $email ?>" method="post">
+                <!-- Utilize o valor da senha gerada no formulário anterior -->
+                <input type="hidden" name="acesso" id="acesso" value="<?php echo $acesso ?>">
+            </form>
+        </div>
+
+        <script>
+            function submitForm() {
+                // Não é necessário gerar uma nova senha aqui, apenas enviar o formulário
+                document.getElementById('cadastroForm').submit();
+            }
+        </script>
+    </body>
+    </html>
+
+    <?php
 
     // Hash a senha antes de armazenar no banco de dados
     $senha_hash = password_hash($acesso, PASSWORD_DEFAULT);
@@ -62,7 +90,7 @@
     $update_setor = mysqli_query($conexao, "UPDATE setor SET quantidade = quantidade + 1 WHERE id_setor = '$setor'");
 
  
-    $criar_funionario = mysqli_query($conexao, "INSERT INTO funcionario(nome_funcionario, nacionalidade, data_nascimento, cpf, rg, email, celular, cidade, endereco, pcd, foto_funcionario, id_setor, id_login) 
+    $criar_funcionario = mysqli_query($conexao, "INSERT INTO funcionario(nome_funcionario, nacionalidade, data_nascimento, cpf, rg, email, celular, cidade, endereco, pcd, foto_funcionario, id_setor, id_login) 
     VALUES('$nome_funcionario', '$nacionalidade', '$data_nascimento', '$cpf', '$rg', '$email', '$celular', '$cidade', '$endereco', '$pcd', '$imagem_destino', '$setor', '$id_login')");
 
     

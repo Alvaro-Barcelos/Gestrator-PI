f88bb532c8cdf133d0ef14bd7f113aea44342eee
11082005 <112984564+11082005@users.noreply.github.com>
2024-07-31 16:46:39 -0300

    home adm button

--- dashtreme-master/project/html/home_adm.php
+++ dashtreme-master/project/html/home_adm.php
@@ -487,9 +487,13 @@ i.fa-comment {
 <!--Start topbar header-->
 <header class="topbar-nav">
  <nav class="navbar navbar-expand fixed-top">
-  <ul class="navbar-nav mr-auto align-items-center">
-   
-  </ul>
+ <ul class="navbar-nav mr-auto align-items-center">
+      <li class="nav-item">
+        <a class="nav-link toggle-menu" href="javascript:void();">
+          <i class="icon-menu menu-icon"></i>
+        </a>
+      </li>
+    </ul>
      
   <ul class="navbar-nav align-items-center right-nav-link">
     
